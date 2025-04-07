5bebbd63f077f0bc28dce38af362c2bf112818b5
S A M E E R <sameerakramsameerakram01@gmail.com>
2025-04-07 15:29:24 +0500

    add 3d Equipments

---
+++ src/app/components/cloth/cloth.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cloth',
+  imports: [],
+  templateUrl: './cloth.component.html',
+  styleUrl: './cloth.component.css'
+})
+export class ClothComponent {
+
+}
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { SliderComponent } from '../slider/slider.component';
+import { ClothComponent } from '../cloth/cloth.component';
 
 @Component({
   selector: 'app-home',
-  imports: [SliderComponent],
+  imports: [SliderComponent,ClothComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
